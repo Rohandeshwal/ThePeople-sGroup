505e26750cf8f6ad85d9f39a54b03bb3f5108271
Rohan <rohandeshwal19@gmail.com>
2022-01-03 13:07:19 +0530

    set index.html

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+
+
+const routes: Routes = [
+  // {path:'',redirectTo:'home',pathMatch:'full'},
+  // {path:'home', loadChildren:() => import('./modules/home/home.module').then(m => m.HomeModule)},
+  {
+    path: "",
+    redirectTo: "home",
+    pathMatch: "full"
+  },
+//   { path: "home", component: HomeComponent},
+//   { path: "service/:id", component: RegisterServiceComponent,
+  
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
