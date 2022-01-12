537a6c5b1f89103574cdc998d61dbbad4214d926
Rohan <rohandeshwal19@gmail.com>
2022-01-12 22:41:13 +0530

    msg

--- src/app/modules/home/components/events-listing/events-listing.component.ts
+++ src/app/modules/home/components/events-listing/events-listing.component.ts
@@ -6,10 +6,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./events-listing.component.scss']
 })
 export class EventsListingComponent implements OnInit {
+  cardsData:any[] =[
 
+        {
+          "id": 1,
+          "img": "./assets/logo/peoplesGroup.PNG",
+          "name": "EVENT1",
+          "description": "Offices, schools, nursing homes, shopping complex and other commercial settings are especially susceptible to germs and bacterial infestations, and not just during the cold and flu seasons.",
+        },
+        {
+          "id": 2,
+          "img": "./assets/logo/peoplesGroup.PNG",
+          "name": "EVENT2",
+          "description": "Offices, schools, nursing homes, shopping complex and other commercial settings are especially susceptible to germs and bacterial infestations, and not just during the cold and flu seasons.",
+        },
+        {
+          "id": 3,
+          "img": "./assets/logo/peoplesGroup.PNG",
+          "name": "EVENT3",
+          "description": "Offices, schools, nursing homes, shopping complex and other commercial settings are especially susceptible to germs and bacterial infestations, and not just during the cold and flu seasons.",
+        },
+        {
+          "id": 4,
+          "img": "./assets/logo/peoplesGroup.PNG",
+          "name": "EVENT4",
+          "description": "Offices, schools, nursing homes, shopping complex and other commercial settings are especially susceptible to germs and bacterial infestations, and not just during the cold and flu seasons.",
+        }
+
+    ];
   constructor() { }
 
   ngOnInit(): void {
   }
+  
 
 }
